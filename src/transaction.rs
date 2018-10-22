0f842cd15b96745c849fbd86c9e4f956af4e46df
Cathie Yun <cathieyun@gmail.com>
2018-10-22 16:20:15 -0700

    add imports

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,1 +1,14 @@
+use gadgets::{k_merge, k_split, k_value_shuffle, pad, range_proof};
+use bulletproofs::r1cs::{Assignment, ConstraintSystem, Variable};
+use curve25519_dalek::scalar::Scalar;
+use util::{SpacesuitError, Value};
 
+// Enforces that the outputs are a valid rearrangement of the inputs, following the 
+// soundness and secrecy requirements in the spacesuit spec.
+pub fn fill_cs<CS: ConstraintSystem>(
+    cs: &mut CS,
+    inputs: Vec<Value>,
+    outputs: Vec<Value>,
+) -> Result<(), SpacesuitError> {
+    unimplemented!();
+}
