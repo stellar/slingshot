5c06a6dc7c8eaafdcd457bbfe18f3c5798f6b252
Cathie Yun <cathieyun@gmail.com>
2018-10-26 15:44:51 -0700

    adding more comments

--- src/gadgets/split.rs
+++ src/gadgets/split.rs
@@ -3,8 +3,12 @@ use bulletproofs::r1cs::ConstraintSystem;
 use util::{SpacesuitError, Value};
 
 /// Enforces that the outputs are either a split of the inputs :`A = C + D && B = 0`,
-/// or the outputs are equal to the inputs `C = A && D = B`.
+/// or the outputs are equal to the inputs `C = A && D = B`. See spec for more details.
 /// Works for `k` inputs and `k` outputs.
+///
+/// Note: the `split` gadget is the same thing as a `merge` gadget, but "backwards".
+/// This means that if you reverse all of the commitment vectors, and switch the 
+/// inputs and outputs of a `merge` gadget, then you have a `split` gadget.
 pub fn fill_cs<CS: ConstraintSystem>(
     cs: &mut CS,
     inputs: Vec<Value>,
