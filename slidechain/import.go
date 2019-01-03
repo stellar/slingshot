71ca2041a3744f9b46bc761d74414f6ae42bc987
Vicki Niu <vicki.niu@gmail.com>
2019-01-03 15:44:23 -0500

    use ForQueryRows, update db on each row

--- slidechain/import.go
+++ slidechain/import.go
@@ -3,30 +3,23 @@ package main
 import (
 	"context"
 	"database/sql"
+
+	"github.com/chain/chain/database/pg"
 )
 
 func importFromPegs(ctx context.Context, db *sql.DB) error {
 	for {
 		var importedIDs []string
-		var q = `SELECT (txid, txhash, operation_num, amount, asset_code) FROM pegs WHERE imported=0`
-		rows, err := db.QueryContext(ctx, q)
-		defer rows.Close()
-		for rows.Next() {
-			var (
-				txid                 string
-				txhash, assetCode    []byte
-				operationNum, amount int
-			)
-			err := rows.Scan(&txid, &txhash, &operationNum, &amount, &assetCode)
+
+		const q = `SELECT txid, txhash, operation_num, amount, asset_code FROM pegs WHERE imported=0`
+		err := pg.ForQueryRows(ctx, db, q, func(txid string, txhash []byte, operationNum, amount int, assetCode []byte) error {
+			// TODO: import the specified row through issuance contract
+			_, err := db.ExecContext(ctx, `UPDATE pegs SET imported=1 WHERE txid=$1`, txid)
 			if err != nil {
 				return err
 			}
-			importedIDs = append(importedIDs, txid)
-			// TODO: import the specified row through issuance contract
-		}
-		_, err := db.ExecContext(ctx, `UPDATE pegs SET imported = 1 WHERE txid=unnest($1::text[])`, importedIDs)
-		if err != nil {
-			return err
-		}
+		})
+		return err
 	}
+	return nil
 }
