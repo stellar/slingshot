03f1838cb9623e890e5f94bcd343750e0faa7fa0
Debnil Sur <debnil@chain.com>
2019-01-08 16:08:35 -0800

    actually fix whitespace, move issuance assembly

--- slidechain/contracts.go
+++ slidechain/contracts.go
@@ -17,12 +17,12 @@ func mustAssemble(src string) []byte {
 
 // TODO(debnil): Use a more general-purpose sig checker, i.e. an exported `multisigProgCheckSrc`.
 const issueProgFmt = `
-                                                    #  con stack                    arg stack                        log                                              notes
-                                                    #  ---------                    ---------                        ---                                              -----
-                                                    #                               asset code, amount, zeroval                                                       
-[txid <pubkey> get 0 checksig verify] contract put  #                               sigcheck contract                                                                 
-get get get                                         #  zeroval, amount, asset code                                                                                    
-issue put                                           #                               sigcheck contract, issued value  {"A", vm.caller, v.amount, v.assetid, v.anchor}  
+	                                                    #  con stack                    arg stack                        log                                              notes
+	                                                    #  ---------                    ---------                        ---                                              -----
+	                                                    #                               asset code, amount, zeroval                                                       
+	get get get                                         #  zeroval, amount, asset code                                                                                    
+	[txid <pubkey> get 0 checksig verify] contract put  #                               sigcheck contract                                                                 
+	issue put                                           #                               sigcheck contract, issued value  {"A", vm.caller, v.amount, v.assetid, v.anchor}  
 `
 
 var (
--- slidechain/slidechain.go
+++ slidechain/slidechain.go
@@ -15,6 +15,7 @@ import (
 	"github.com/chain/txvm/errors"
 	"github.com/chain/txvm/protocol"
 	"github.com/chain/txvm/protocol/bc"
+	"github.com/chain/txvm/protocol/txvm"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stellar/go/clients/horizon"
 	"github.com/stellar/go/xdr"
@@ -23,7 +24,6 @@ import (
 var (
 	initialBlock *bc.Block
 	chain        *protocol.Chain
-	custPubkey   = flag.String("custpubkey", "", "custodian txvm pubkey")
 )
 
 type custodian struct {
@@ -75,13 +75,19 @@ func main() {
 	ctx := context.Background()
 
 	var (
-		addr   = flag.String("addr", "localhost:2423", "server listen address")
-		dbfile = flag.String("db", "slidechain.db", "path to db")
-		custID = flag.String("custid", "", "custodian's Stellar account ID")
-		url    = flag.String("horizon", "https://horizon-testnet.stellar.org", "horizon server url")
+		addr          = flag.String("addr", "localhost:2423", "server listen address")
+		dbfile        = flag.String("db", "slidechain.db", "path to db")
+		custID        = flag.String("custid", "", "custodian's Stellar account ID")
+		url           = flag.String("horizon", "https://horizon-testnet.stellar.org", "horizon server url")
+		custPubkeyHex = flag.String("custpubkey", "", "custodian txvm public key (hex string)")
 	)
 
 	flag.Parse()
+
+	// Assemble issuance TxVM program for custodian.
+	issueProgSrc = fmt.Sprintf(issueProgFmt, *custPubkeyHex)
+	issueProg = mustAssemble(issueProgSrc)
+	issueSeed = txvm.ContractSeed(issueProg)
 
 	var cur horizon.Cursor // TODO: initialize from db (if applicable)
 
