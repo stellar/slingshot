b1057c51dc92feacb38da9d5bfb8fbaa44596066
Debnil Sur <debnil@chain.com>
2019-01-08 16:22:12 -0800

    simplify declaration for issuance variables

--- slidechain/contracts.go
+++ slidechain/contracts.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/chain/txvm/protocol/txvm"
 	"github.com/chain/txvm/protocol/txvm/asm"
 )
-
-func mustAssemble(src string) []byte {
-	res, err := asm.Assemble(src)
-	if err != nil {
-		panic(err)
-	}
-	return res
-}
 
 // TODO(debnil): Use a more general-purpose sig checker, i.e. an exported `multisigProgCheckSrc`.
 const issueProgFmt = `
@@ -26,7 +15,15 @@ const issueProgFmt = `
 `
 
 var (
-	issueProgSrc = fmt.Sprintf(issueProgFmt, *custPubkey)
-	issueProg    = mustAssemble(issueProgSrc)
-	issueSeed    = txvm.ContractSeed(issueProg)
+	issueProgSrc string
+	issueProg    []byte
+	issueSeed    [32]byte
 )
+
+func mustAssemble(src string) []byte {
+	res, err := asm.Assemble(src)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
