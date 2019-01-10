73f74d12cddc68ba35e7f50879e2f45c55a78dd3
Vicki Niu <vicki.niu@gmail.com>
2019-01-10 18:08:24 -0500

    slidechain: fix db bugs (#15)
    Fixes bugs in query syntax and accessing the db while locked.

--- slidechain/import.go
+++ slidechain/import.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/bobg/sqlutil"
+	"github.com/chain/txvm/errors"
 )
 
 func (c *custodian) importFromPegs(ctx context.Context, s *submitter) error {
@@ -11,15 +12,24 @@ func (c *custodian) importFromPegs(ctx context.Context, s *submitter) error {
 	defer c.imports.L.Unlock()
 	for {
 		c.imports.Wait()
+		var (
+			txids                  []string
+			operationNums, amounts []int
+			assets                 [][]byte
+		)
 		const q = `SELECT txid, txhash, operation_num, amount, asset FROM pegs WHERE imported=0`
-		err := sqlutil.ForQueryRows(ctx, c.db, q, func(txid string, txhash []byte, operationNum, amount int, asset []byte) error {
-			// TODO: import the specified row through issuance contract
-			_, err := c.db.ExecContext(ctx, `UPDATE pegs SET imported=1 WHERE txid=$1`, txid)
-			if err != nil {
-				return err
-			}
-			return nil
+		err := sqlutil.ForQueryRows(ctx, c.db, q, func(txid string, txhash []byte, operationNum, amount int, asset []byte) {
+			txids = append(txids, txid)
+			operationNums = append(operationNums, operationNum)
+			amounts = append(amounts, amount)
+			assets = append(assets, asset)
 		})
-		return err
+		// TODO: import the specified row through issuance contract
+		for _, txid := range txids {
+			_, err = c.db.ExecContext(ctx, `UPDATE pegs SET imported=1 WHERE txid=$1`, txid)
+			if err != nil {
+				return errors.Wrapf(err, "updating record for tx %s", txid)
+			}
+		}
 	}
 }
