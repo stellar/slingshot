c1ea446e28beba0f20aaafcd7e3fbd5c72a9a180
Debnil Sur <debnil@chain.com>
2019-01-16 16:38:17 -0800

    most of bob's initial review

--- slidechain/atomic.go
+++ slidechain/atomic.go
@@ -11,8 +11,8 @@ import (
 const atomicGuaranteeSrc = `
                                              #  con stack       arg stack       log
                                              #  ---------       ---------       ---
-                                             #                  recip, zeroval
-        get get [drop get eq verify] output  #  zeroval, recip                  {"O", vm.caller, outputid}
+                                             #                  zeroval, recip
+        get get [drop get eq verify] output  #  recip, zeroval                  {"O", vm.caller, outputid}
 `
 
 var (
@@ -22,22 +22,28 @@ var (
 
 // InputAtomic writes txvm bytecode to b, calling the atomicity-guarantee contract
 // to confirm that the desired recipient pubkey is present.
-func InputAtomic(b *txvmutil.Builder, pubkey ed25519.PublicKey, seed []byte) {
-	Snapshot(b, pubkey, seed)
+func InputAtomic(b *txvmutil.Builder, pubkey ed25519.PublicKey) {
+	Snapshot(b, pubkey)
 	b.Op(op.Input).Op(op.Call)
 }
 
 // Snapshot adds to b the snapshot of an atomicity-guarantee contract as it appears in the UTXO set.
-func Snapshot(b *txvmutil.Builder, pubkey ed25519.PublicKey, seed []byte) {
+// TODO(debnil): Add anchor parameter.
+func Snapshot(b *txvmutil.Builder, pubkey ed25519.PublicKey) {
 	b.Tuple(func(contract *txvmutil.TupleBuilder) {
 		contract.PushdataByte(txvm.ContractCode)          // 'C'
-		contract.PushdataBytes(seed)                      // <seed>
-		contract.PushdataBytes(atomicGuaranteeProg)       // [<atomicity issuance prog>]
+		contract.PushdataBytes(atomicGuaranteeSeed[:])    // <atomic guarantee seed>
+		contract.PushdataBytes(atomicGuaranteeProg)       // [<atomicity guarantee prog>]
 		contract.Tuple(func(tup *txvmutil.TupleBuilder) { // {'S', pubkey}
 			tup.PushdataByte(txvm.BytesCode)
 			tup.Tuple(func(pktup *txvmutil.TupleBuilder) {
 				pktup.PushdataBytes(pubkey)
 			})
+		})
+		contract.Tuple(func(tup *txvmutil.TupleBuilder) {
+			tup.PushdataByte(txvm.ValueCode)
+			tup.PushdataInt64(0)
+			// TODO(debnil): Add zero hex string and anchor.
 		})
 	})
 }
