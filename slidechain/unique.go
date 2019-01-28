4285f00cdd5a12327731b3f94106e6779af551f0
Debnil Sur <debnil@chain.com>
2019-01-28 14:28:43 -0800

    Remove nonce hash function - not used yet.

--- slidechain/unique.go
+++ slidechain/unique.go
@@ -48,10 +48,3 @@ var (
 	importUniquenessSeed = txvm.ContractSeed(importUniquenessProg)
 	zeroSeed             [32]byte
 )
-
-// UniqueNonceHash returns a nonce hash that can be used before pegging
-// and after importing to prevent replay attacks.
-func UniqueNonceHash(bcid []byte, expMS int64) [32]byte {
-	nonce := txvm.NonceTuple(zeroSeed[:], zeroSeed[:], bcid, expMS)
-	return txvm.NonceHash(nonce)
-}
