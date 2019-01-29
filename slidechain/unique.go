e3a739b0c5f20101b013fb790fb712b5016fa195
Debnil Sur <debnil@chain.com>
2019-01-28 16:30:32 -0800

    try to fix tabbing

--- slidechain/unique.go
+++ slidechain/unique.go
@@ -18,23 +18,23 @@ const (
 `
 
 	importUniquenessFmt = `
-						 #  con stack                                                                arg stack                                log  notes
-						 #  ---------                                                                ---------                                ---  -----
-		 				 #  quorum, {recip}, zeroval, amount, asset                                                                                
-	caller               #  quorum, {recip}, zeroval, amount, asset, callerSeed                                                                    
-	%s					 #  quorum, {recip}, zeroval, amount, asset, callerSeed, importIssuanceSeed                                                
-	eq verify            #  quorum, {recip}, zeroval, amount, asset                                                                                
-	put put put put put  #                                                                           asset, amount, zeroval, {recip}, quorum       
+							#  con stack                                                                arg stack                                log  notes
+							#  ---------                                                                ---------                                ---  -----
+							#  quorum, {recip}, zeroval, amount, asset                                                                                
+	caller					#  quorum, {recip}, zeroval, amount, asset, callerSeed                                                                    
+	%s						#  quorum, {recip}, zeroval, amount, asset, callerSeed, importIssuanceSeed                                                
+	eq verify				#  quorum, {recip}, zeroval, amount, asset                                                                                
+	put put put put put		#                                                                           asset, amount, zeroval, {recip}, quorum       
 `
 
 	importIssuanceSrc = `
 														#  con stack                                arg stack                                log                                              notes
 														#  ---------                                ---------                                ---                                              -----
 														#                                           importUniquenessContract                                                                  
-	get call                                            #                                           asset, amount, zeroval, {recip}, quorum                                                   
-	get get get get get                                 #  quorum, {recip}, zeroval, amount, asset                                                                                            
-	[txid <pubkey> get 0 checksig verify] contract put  #  quorum, {recip}, zeroval, amount, asset  sigchecker                                                                                
-	issue put put put                                   #                                           sigchecker, issuedval, {recip}, quorum   {"A", vm.caller, v.amount, v.assetid, v.anchor}
+	get call											#                                           asset, amount, zeroval, {recip}, quorum                                                   
+	get get get get get									#  quorum, {recip}, zeroval, amount, asset                                                                                            
+	[txid <pubkey> get 0 checksig verify] contract put	#  quorum, {recip}, zeroval, amount, asset  sigchecker                                                                                
+	issue put put put									#                                           sigchecker, issuedval, {recip}, quorum   {"A", vm.caller, v.amount, v.assetid, v.anchor}
 `
 )
 
