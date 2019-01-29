56f18a298935d62ed775780ce8db90379108297b
Debnil Sur <debnil@chain.com>
2019-01-28 16:38:08 -0800

    redo programs to fix whitespace

--- slidechain/unique.go
+++ slidechain/unique.go
@@ -9,32 +9,32 @@ import (
 
 const (
 	prepegUniquenessFmt = `
-							#  con stack                                                        arg stack                                log                         notes
-							#  ---------                                                        ---------                                ---                         -----
-							#                                                                   asset, amount, zeroval, {recip}, quorum                              
+	                        #  con stack                                                        arg stack                                log                         notes
+	                        #  ---------                                                        ---------                                ---                         -----
+	                        #                                                                   asset, amount, zeroval, {recip}, quorum                              
 	get get get get get     #  quorum, {recip}, zeroval, amount, asset                                                                                               
 	[%s]                    #  quorum, {recip}, zeroval, amount, asset, [importUniquenessProg]                                                                       
 	output                  #  quorum, {recip}, zeroval, amount, asset                                                                   {"O", vm.caller, outputid}  
 `
 
 	importUniquenessFmt = `
-							#  con stack                                                                arg stack                                log  notes
-							#  ---------                                                                ---------                                ---  -----
-							#  quorum, {recip}, zeroval, amount, asset                                                                                
-	caller					#  quorum, {recip}, zeroval, amount, asset, callerSeed                                                                    
-	%s						#  quorum, {recip}, zeroval, amount, asset, callerSeed, importIssuanceSeed                                                
-	eq verify				#  quorum, {recip}, zeroval, amount, asset                                                                                
-	put put put put put		#                                                                           asset, amount, zeroval, {recip}, quorum       
+	                     #  con stack                                                                arg stack                                log  notes
+	                     #  ---------                                                                ---------                                ---  -----
+	                     #  quorum, {recip}, zeroval, amount, asset                                                                                
+	caller               #  quorum, {recip}, zeroval, amount, asset, callerSeed                                                                    
+	%s               	 #  quorum, {recip}, zeroval, amount, asset, callerSeed, importIssuanceSeed                                                
+	eq verify            #  quorum, {recip}, zeroval, amount, asset                                                                                
+	put put put put put  #                                                                           asset, amount, zeroval, {recip}, quorum             
 `
 
 	importIssuanceSrc = `
-														#  con stack                                arg stack                                log                                              notes
-														#  ---------                                ---------                                ---                                              -----
-														#                                           importUniquenessContract                                                                  
-	get call											#                                           asset, amount, zeroval, {recip}, quorum                                                   
-	get get get get get									#  quorum, {recip}, zeroval, amount, asset                                                                                            
-	[txid <pubkey> get 0 checksig verify] contract put	#  quorum, {recip}, zeroval, amount, asset  sigchecker                                                                                
-	issue put put put									#                                           sigchecker, issuedval, {recip}, quorum   {"A", vm.caller, v.amount, v.assetid, v.anchor}
+	                                                    #  con stack                                arg stack                                log                                              notes
+	                                                    #  ---------                                ---------                                ---                                              -----
+	                                                    #                                           importUniquenessContract                                                                  
+	get call                                            #                                           asset, amount, zeroval, {recip}, quorum                                                   
+	get get get get get                                 #  quorum, {recip}, zeroval, amount, asset                                                                                            
+	[txid <pubkey> get 0 checksig verify] contract put  #  quorum, {recip}, zeroval, amount, asset  sigchecker                                                                                
+	issue put put put                                   #                                           sigchecker, issuedval, {recip}, quorum   {"A", vm.caller, v.amount, v.assetid, v.anchor}
 `
 )
 
