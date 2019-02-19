a48798eb066f5d946b5e8ec6119d3582d1e833de
Vicki Niu <vicki.niu@gmail.com>
2019-02-19 18:16:12 -0500

    use unwrap, add separate recipient pred

--- zkvm/tests/zkvm.rs
+++ zkvm/tests/zkvm.rs
@@ -12,46 +12,48 @@ fn issue_contract(
     flv: Scalar,
     issuance_pred: &Predicate,
     nonce_pred: &Predicate,
+    recipient_pred: &Predicate,
 ) -> Vec<Instruction> {
     vec![
         Instruction::Push(Commitment::from(CommitmentWitness::blinded(qty)).into()), // stack: qty
         Instruction::Var, // stack: qty-var
         Instruction::Push(Commitment::from(CommitmentWitness::unblinded(flv)).into()), // stack: qty-var, flv
-        Instruction::Var,                                // stack: qty-var, flv-var
-        Instruction::Push(issuance_pred.clone().into()), // stack: qty-var, flv-var, pred
-        Instruction::Issue,                              // stack: issue-contract
-        Instruction::Push(nonce_pred.clone().into()),    // stack: issue-contract, pred
-        Instruction::Nonce,                              // stack: issue-contract, nonce-contract
-        Instruction::Signtx,                             // stack: issue-contract
-        Instruction::Signtx,                             // stack: issued-value
-        Instruction::Push(issuance_pred.clone().into()), // stack: issued-value, pred
-        Instruction::Output(1),                          // stack: empty
+        Instruction::Var,                                 // stack: qty-var, flv-var
+        Instruction::Push(issuance_pred.clone().into()),  // stack: qty-var, flv-var, pred
+        Instruction::Issue,                               // stack: issue-contract
+        Instruction::Push(nonce_pred.clone().into()),     // stack: issue-contract, pred
+        Instruction::Nonce,                               // stack: issue-contract, nonce-contract
+        Instruction::Signtx,                              // stack: issue-contract
+        Instruction::Signtx,                              // stack: issued-value
+        Instruction::Push(recipient_pred.clone().into()), // stack: issued-value, pred
+        Instruction::Output(1),                           // stack: empty
     ]
 }
 
 #[test]
 fn issue() {
     let (tx, txid) = {
-        let issuance_pred = match Predicate::from_witness(PredicateWitness::Key(Scalar::random(
+        let issuance_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
             &mut rand::thread_rng(),
-        ))) {
-            Err(_) => return assert!(false),
-            Ok(x) => x,
-        };
+        )))
+        .unwrap();
 
-        let nonce_pred = match Predicate::from_witness(PredicateWitness::Key(Scalar::random(
+        let nonce_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
             &mut rand::thread_rng(),
-        ))) {
-            Err(_) => return assert!(false),
-            Ok(x) => x,
-        };
+        )))
+        .unwrap();
+
+        let recipient_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
+            &mut rand::thread_rng(),
+        )))
+        .unwrap();
 
         // Generate flavor scalar
         let mut t = Transcript::new(b"ZkVM.issue");
         t.commit_bytes(b"predicate", issuance_pred.point().as_bytes());
         let flavor = t.challenge_scalar(b"flavor");
 
-        let program = issue_contract(1u64, flavor, &issuance_pred, &nonce_pred);
+        let program = issue_contract(1u64, flavor, &issuance_pred, &nonce_pred, &recipient_pred);
 
         let bp_gens = BulletproofGens::new(64, 1);
         // TBD: add TxHeader type to make this call more readable
