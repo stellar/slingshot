332d640242c5de689dba6b4d794203a99e17d34c
Vicki Niu <vicki.niu@gmail.com>
2019-02-20 10:58:33 -0500

    check txid value

--- zkvm/tests/zkvm.rs
+++ zkvm/tests/zkvm.rs
@@ -1,5 +1,6 @@
 use bulletproofs::BulletproofGens;
 use curve25519_dalek::scalar::Scalar;
+use hex;
 use merlin::Transcript;
 
 use zkvm::*;
@@ -12,7 +13,13 @@ fn issue_contract(
     recipient_pred: &Predicate,
 ) -> Vec<Instruction> {
     vec![
-        Instruction::Push(Commitment::from(CommitmentWitness::blinded(qty)).into()), // stack: qty
+        Instruction::Push(
+            Commitment::from(CommitmentWitness {
+                value: qty.into(),
+                blinding: Scalar::from(1u64),
+            })
+            .into(),
+        ), // stack: qty
         Instruction::Var, // stack: qty-var
         Instruction::Push(Commitment::from(CommitmentWitness::unblinded(flv)).into()), // stack: qty-var, flv
         Instruction::Var,                                 // stack: qty-var, flv-var
@@ -29,29 +36,24 @@ fn issue_contract(
 
 #[test]
 fn issue() {
-    let (tx, _, txlog) = {
-        let issuance_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
-            &mut rand::thread_rng(),
-        )))
-        .unwrap();
-
-        let nonce_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
-            &mut rand::thread_rng(),
-        )))
-        .unwrap();
-
-        let recipient_pred = Predicate::from_witness(PredicateWitness::Key(Scalar::random(
-            &mut rand::thread_rng(),
-        )))
-        .unwrap();
+    let (tx, txid, txlog) = {
+        // Generate predicates
+        let issuance_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(0u64))).unwrap();
+        let nonce_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(1u64))).unwrap();
+        let recipient_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(2u64))).unwrap();
 
         // Generate flavor scalar
         let mut t = Transcript::new(b"ZkVM.issue");
         t.commit_bytes(b"predicate", issuance_pred.point().as_bytes());
         let flavor = t.challenge_scalar(b"flavor");
 
+        // Build program
         let program = issue_contract(1u64, flavor, &issuance_pred, &nonce_pred, &recipient_pred);
 
+        // Build tx
         let bp_gens = BulletproofGens::new(64, 1);
         // TBD: add TxHeader type to make this call more readable
         let txresult = Prover::build_tx(program, 0u64, 0u64, 0u64, &bp_gens);
@@ -61,6 +63,12 @@ fn issue() {
         };
         (tx, txid, txlog)
     };
+
+    // Check txid
+    assert_eq!(
+        "9d5c0c4f47b894bca9432a2ffbaf21303731cb08d8111e40b004264c3e20d35d",
+        hex::encode(txid.0)
+    );
 
     // Verify tx
     let bp_gens = BulletproofGens::new(64, 1);
