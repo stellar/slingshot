7d5cb87f21498a6087465d2b93b2fd9780372e3a
Cathie Yun <cathieyun@gmail.com>
2019-02-20 14:33:14 -0800

    add spend_1_2 with cloak call, does not pass

--- zkvm/tests/zkvm.rs
+++ zkvm/tests/zkvm.rs
@@ -89,6 +89,7 @@ fn issue() {
 
 // questions:
 // - do we have to use separate nonce predicates for the two inputs (to ensure uniqueness if they're same qty?)
+// - is it enough (for testing) to get the value from the issue contract? (how do we create a "utxo" in one tx?)
 fn spend_2_2_contract(
     input_1: u64,
     input_2: u64,
@@ -135,6 +136,95 @@ fn spend_2_2() {
         let program = spend_2_2_contract(
             6u64,
             4u64,
+            9u64,
+            1u64,
+            flavor,
+            &issuance_pred,
+            &nonce_pred,
+            &recipient_1_pred,
+            &recipient_2_pred,
+        );
+
+        // Build tx
+        let bp_gens = BulletproofGens::new(64, 1);
+        // TBD: add TxHeader type to make this call more readable
+        let txresult = Prover::build_tx(program, 0u64, 0u64, 0u64, &bp_gens);
+        let (tx, txid, txlog) = match txresult {
+            Err(err) => return assert!(false, err.to_string()),
+            Ok(x) => x,
+        };
+        (tx, txid, txlog)
+    };
+
+    // Verify tx
+    let bp_gens = BulletproofGens::new(64, 1);
+    match Verifier::verify_tx(tx, &bp_gens) {
+        Err(err) => return assert!(false, err.to_string()),
+        Ok(v) => {
+            assert_eq!(v.log, txlog);
+        }
+    };
+}
+
+fn spend_1_2_contract(
+    input_1: u64,
+    output_1: u64,
+    output_2: u64,
+    flv: Scalar,
+    issuance_pred: &Predicate,
+    nonce_pred: &Predicate,
+    recipient_1_pred: &Predicate,
+    recipient_2_pred: &Predicate,
+) -> Vec<Instruction> {
+    let mut instructions = vec![];
+    instructions.append(&mut issue_helper(input_1, flv, issuance_pred, nonce_pred)); // stack: issued-value
+
+    // TODO: just use the blinded() function instead?
+    instructions.push(Instruction::Push(
+        Commitment::from(CommitmentWitness::blinded(output_1)).into(),
+    )); // stack: issued-value, output-1-quantity
+    instructions.push(Instruction::Push(
+        Commitment::from(CommitmentWitness::blinded(flv)).into(),
+    )); // stack: issued-value, output-1-quantity, output-1-flavor
+
+    instructions.push(Instruction::Push(
+        Commitment::from(CommitmentWitness::blinded(output_2)).into(),
+    )); // stack: ... output-2-quantity
+    instructions.push(Instruction::Push(
+        Commitment::from(CommitmentWitness::blinded(flv)).into(),
+    )); // stack: ... output-2-quantity, output-2-flavor
+
+    instructions.push(Instruction::Cloak(1, 2)); // stack: output-1, output-2
+
+    instructions.push(Instruction::Push(recipient_2_pred.clone().into())); // stack: output-1, output-2, recipient-2-pred
+    instructions.push(Instruction::Output(1)); // stack: output-1
+    instructions.push(Instruction::Push(recipient_1_pred.clone().into())); // stack: output-1, recipient-1-pred
+    instructions.push(Instruction::Output(1)); // stack: empty
+
+    instructions
+}
+
+#[test]
+fn spend_1_2() {
+    let (tx, _txid, txlog) = {
+        // Generate predicates
+        let issuance_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(0u64))).unwrap();
+        let nonce_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(1u64))).unwrap();
+        let recipient_1_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(2u64))).unwrap();
+        let recipient_2_pred =
+            Predicate::from_witness(PredicateWitness::Key(Scalar::from(3u64))).unwrap();
+
+        // Generate flavor scalar
+        let mut t = Transcript::new(b"ZkVM.issue");
+        t.commit_bytes(b"predicate", issuance_pred.point().as_bytes());
+        let flavor = t.challenge_scalar(b"flavor");
+
+        // Build program
+        let program = spend_1_2_contract(
+            10u64,
             9u64,
             1u64,
             flavor,
