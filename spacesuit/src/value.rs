b5b567600d6d7561984083caa91ec18e6658dc0d
decentralisedkev <37423678+decentralisedkev@users.noreply.github.com>
2019-02-23 00:54:08 +0000

    zkvm: formalize constant expressions with an enum (#98)

--- spacesuit/src/value.rs
+++ spacesuit/src/value.rs
@@ -34,7 +34,7 @@ pub struct AllocatedQuantity {
 }
 
 /// Represents a signed integer with absolute value in the 64-bit range.
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd)]
 pub struct SignedInteger(i128);
 
 impl Value {
@@ -106,6 +106,10 @@ impl SignedInteger {
         } else {
             Some(self.0 as u64)
         }
+    }
+
+    pub fn to_scalar(self) -> Scalar {
+        self.into()
     }
 }
 
