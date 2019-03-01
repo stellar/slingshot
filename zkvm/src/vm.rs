27b7ed8d802369d7e3482b55941e6f75efe3c8e7
decentralisedkev <37423678+decentralisedkev@users.noreply.github.com>
2019-03-01 15:48:17 +0000

    zkvm: Add rangeproof instruction (#166)
    Closes #151

--- zkvm/src/vm.rs
+++ zkvm/src/vm.rs
@@ -206,7 +206,7 @@ where
                 Instruction::Add => self.add()?,
                 Instruction::Mul => self.mul()?,
                 Instruction::Eq => self.eq()?,
-                Instruction::Range(_) => unimplemented!(),
+                Instruction::Range(i) => self.range(i)?,
                 Instruction::And => self.and()?,
                 Instruction::Or => self.or()?,
                 Instruction::Verify => self.verify()?,
@@ -314,6 +314,16 @@ where
         let expr1 = self.pop_item()?.to_expression()?;
         let constraint = Constraint::Eq(expr1, expr2);
         self.push_item(constraint);
+        Ok(())
+    }
+
+    fn range(&mut self, i: u8) -> Result<(), VMError> {
+        if i < 1 || i > 64 {
+            return Err(VMError::InvalidBitrange);
+        }
+        let expr = self.pop_item()?.to_expression()?;
+        self.add_range_proof(i as usize, expr.clone())?;
+        self.push_item(expr);
         Ok(())
     }
 
