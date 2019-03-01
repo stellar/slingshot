42bb248b2e26400594fe4ba7ceb93a2a4b855562
Debnil Sur <debnil@chain.com>
2019-03-01 15:47:39 -0800

    Cleanup.

--- slidechain/postpegout.go
+++ slidechain/postpegout.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"math"
 
 	"github.com/chain/txvm/crypto/ed25519"
@@ -71,7 +70,6 @@ func (c *Custodian) doPostPegOut(ctx context.Context, assetXDR, anchor, txid []b
 
 	// Check signature.
 	prog1 := b.Build()
-	log.Printf("%x", prog1)
 	vm, err := txvm.Validate(prog1, 3, math.MaxInt64, txvm.StopAfterFinalize)
 	if err != nil {
 		return errors.Wrap(err, "computing transaction ID")
