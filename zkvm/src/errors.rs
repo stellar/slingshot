00779541553960ab0db4099437354c4967b45325
BlockChainDev <kevtheappdev@gmail.com>
2019-03-01 01:46:00 +0000

    Add documentation to remove lint error

--- zkvm/src/errors.rs
+++ zkvm/src/errors.rs
@@ -118,6 +118,7 @@ pub enum VMError {
     #[fail(display = "Data item must be opaque")]
     DataNotOpaque,
 
+    /// This error occurs when we supply a number not in the range [1,64]
     #[fail(display = "Bitrange for rangeproof is not between 1 and 64")]
     InvalidBitrange,
 }
