90d2ed309ddd3e6445222fba1a2f90098da0e369
BlockChainDev <kevtheappdev@gmail.com>
2019-03-02 00:15:40 +0000

    Add initialiser and into trait

--- spacesuit/src/bit_range.rs
+++ spacesuit/src/bit_range.rs
@@ -1,2 +1,18 @@
 /// Represents a usize with value in the range [0,64]
 pub struct BitRange(usize);
+
+impl BitRange {
+    fn new(n: usize) -> Option<Self> {
+        if n > 64 {
+            None
+        } else {
+            Some(BitRange(n))
+        }
+    }
+}
+
+impl Into<usize> for BitRange {
+    fn into(self) -> usize {
+        self.0
+    }
+}
