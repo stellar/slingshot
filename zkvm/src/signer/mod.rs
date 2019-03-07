a94a78fac420f61f1c01c46c19c6e0cd0328c3c9
Cathie Yun <cathieyun@gmail.com>
2019-03-06 16:42:00 -0800

    add aggregated pubkey test

--- zkvm/src/signer/mod.rs
+++ zkvm/src/signer/mod.rs
@@ -12,7 +12,7 @@ pub mod prover;
 
 pub struct PrivKey(Scalar);
 pub struct PubKey(RistrettoPoint);
-pub struct MultikeyWitness(Vec<PubKey>); // TODO: also include Option<Scalar> for signing key?
+pub struct MultiKey(Vec<PubKey>); // TODO: also include Option<Scalar> for signing key?
 pub struct PubKeyHash(Scalar);
 pub struct Signature {
     s: Scalar,
@@ -31,7 +31,7 @@ pub struct Shared {
     message: Vec<u8>,
 }
 
-impl MultikeyWitness {
+impl MultiKey {
     pub fn aggregate(&self, transcript: &mut Transcript) -> (PubKey, PubKeyHash) {
         // L = H(X_1 || X_2 || ... || X_n)
         for X_i in &self.0 {
@@ -39,12 +39,14 @@ impl MultikeyWitness {
         }
         let L = transcript.challenge_scalar(b"L");
 
-        // X = sum_i ( H(L, X_i) * X_i )
+        // X = sum_i ( a_i * X_i )
+        // a_i = H(L, X_i)
         let mut X = RistrettoPoint::default();
         for X_i in &self.0 {
-            transcript.commit_point(b"X_i.X", &X_i.0.compress());
-            let hash = transcript.challenge_scalar(b"H(L,X_i)");
-            X = X + hash * X_i.0;
+            let mut a_i_transcript = transcript.clone();
+            a_i_transcript.commit_point(b"X_i.X", &X_i.0.compress());
+            let a_i = a_i_transcript.challenge_scalar(b"a_i");
+            X = X + a_i * X_i.0;
         }
 
         (PubKey(X), PubKeyHash(L))
@@ -65,4 +67,50 @@ impl Signature {
         // Check sG = R + H(X,R,m)*X
         self.s * shared.generator == self.R + hash_X_R_m * shared.agg_pubkey.0
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+    use curve25519_dalek::ristretto::CompressedRistretto;
+
+    #[test]
+    fn make_aggregated_pubkey() {
+        // super secret, sshhh!
+        let priv_keys = vec![
+            PrivKey(Scalar::from(1u64)),
+            PrivKey(Scalar::from(2u64)),
+            PrivKey(Scalar::from(3u64)),
+            PrivKey(Scalar::from(4u64)),
+        ];
+        let (pub_key, pub_key_hash) = agg_pubkey_helper(&priv_keys);
+
+        let expected_pub_key = CompressedRistretto::from_slice(&[
+            130, 18, 226, 231, 233, 237, 157, 84, 32, 224, 131, 198, 42, 230, 208, 160, 173, 151,
+            69, 90, 5, 12, 146, 65, 179, 6, 165, 87, 41, 106, 178, 12,
+        ]);
+        let expected_pub_key_hash = Scalar::from_bits([
+            117, 94, 182, 185, 8, 30, 150, 65, 198, 231, 112, 232, 131, 203, 40, 235, 225, 120, 17,
+            229, 216, 77, 98, 51, 230, 53, 250, 192, 247, 82, 234, 3,
+        ]);
+
+        assert_eq!(expected_pub_key, pub_key.0.compress());
+        assert_eq!(expected_pub_key_hash, pub_key_hash.0);
+    }
+
+    fn agg_pubkey_helper(priv_keys: &Vec<PrivKey>) -> (PubKey, PubKeyHash) {
+        let G = RISTRETTO_BASEPOINT_POINT;
+        let multi_key = MultiKey(
+            priv_keys
+                .iter()
+                .map(|priv_key| PubKey(G * priv_key.0))
+                .collect(),
+        );
+        let mut transcript = Transcript::new(b"test");
+        multi_key.aggregate(&mut transcript)
+    }
+
+    #[test]
+    fn sign_message() {}
 }
