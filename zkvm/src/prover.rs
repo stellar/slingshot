f98de4f3a143bf731ffc5a79adf926c71be93770
Oleg Andreev <oleganza@gmail.com>
2019-03-26 14:41:37 -0700

    zkvm|spacesuit: fix for updated bulletproofs API (#257)
    This tweaks the usage of r1cs::Prover/Verifier to the updated API in the develop branch of the Bulletproofs: https://github.com/dalek-cryptography/bulletproofs/pull/257
    
    Folks, run `cargo update` if you have weird compilation errors (after merging this PR).

--- zkvm/src/prover.rs
+++ zkvm/src/prover.rs
@@ -18,16 +18,16 @@ use crate::vm::{Delegate, Tx, TxHeader, VM};
 /// Prover passes the list of instructions through the VM,
 /// creates an aggregated transaction signature (for `signtx` instruction),
 /// creates a R1CS proof and returns a complete `Tx` object that can be published.
-pub struct Prover<'a, 'b> {
+pub struct Prover<'t, 'g> {
     signtx_keys: Vec<VerificationKey>,
-    cs: r1cs::Prover<'a, 'b>,
+    cs: r1cs::Prover<'t, 'g>,
 }
 
 pub(crate) struct ProverRun {
     program: VecDeque<Instruction>,
 }
 
-impl<'a, 'b> Delegate<r1cs::Prover<'a, 'b>> for Prover<'a, 'b> {
+impl<'t, 'g> Delegate<r1cs::Prover<'t, 'g>> for Prover<'t, 'g> {
     type RunType = ProverRun;
 
     fn commit_variable(
@@ -64,19 +64,19 @@ impl<'a, 'b> Delegate<r1cs::Prover<'a, 'b>> for Prover<'a, 'b> {
         })
     }
 
-    fn cs(&mut self) -> &mut r1cs::Prover<'a, 'b> {
+    fn cs(&mut self) -> &mut r1cs::Prover<'t, 'g> {
         &mut self.cs
     }
 }
 
-impl<'a, 'b> Prover<'a, 'b> {
+impl<'t, 'g> Prover<'t, 'g> {
     /// Builds a transaction with a given list of instructions and a `TxHeader`.
     /// Returns a transaction `Tx` along with its ID (`TxID`) and a transaction log (`TxLog`).
     /// Fails if the input program is malformed, or some witness data is missing.
-    pub fn build_tx<'g, F>(
+    pub fn build_tx<F>(
         program: Program,
         header: TxHeader,
-        bp_gens: &'g BulletproofGens,
+        bp_gens: &BulletproofGens,
         sign_tx_fn: F,
     ) -> Result<(Tx, TxID, TxLog), VMError>
     where
@@ -85,7 +85,7 @@ impl<'a, 'b> Prover<'a, 'b> {
         // Prepare the constraint system
         let mut r1cs_transcript = Transcript::new(b"ZkVM.r1cs");
         let pc_gens = PedersenGens::default();
-        let cs = r1cs::Prover::new(bp_gens, &pc_gens, &mut r1cs_transcript);
+        let cs = r1cs::Prover::new(&pc_gens, &mut r1cs_transcript);
 
         // Serialize the tx program
         let mut bytecode = Vec::new();
@@ -113,7 +113,10 @@ impl<'a, 'b> Prover<'a, 'b> {
         let signature = sign_tx_fn(&mut signtx_transcript, &prover.signtx_keys);
 
         // Generate the R1CS proof
-        let proof = prover.cs.prove().map_err(|_| VMError::InvalidR1CSProof)?;
+        let proof = prover
+            .cs
+            .prove(bp_gens)
+            .map_err(|_| VMError::InvalidR1CSProof)?;
 
         Ok((
             Tx {
