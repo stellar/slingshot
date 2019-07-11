bd58bd141aacf7824d0e85820ead442c7bc1ca3a
Andrei <cyberbono3@gmail.com>
2019-07-11 13:11:56 -0700

    cargo fmt

--- spacesuit/src/cloak.rs
+++ spacesuit/src/cloak.rs
@@ -39,7 +39,7 @@ pub fn cloak<CS: RandomizableConstraintSystem>(
             cs,
             output.q.into(),
             output.assignment.map(|v| v.q),
-            BitRange::max()
+            BitRange::max(),
         )?;
     }
 
