255518ccc98914a73bc5e1d38f887566fa89c2fe
Oleg Andreev <oleganza@gmail.com>
2019-07-20 01:59:48 +0300

    wip on key derivation

--- accounts/src/lib.rs
+++ accounts/src/lib.rs
@@ -65,20 +65,23 @@ Questions:
 
 */
 
+use merlin::Transcript;
 use curve25519_dalek::scalar::Scalar;
 use keytree::{Xpub,Xprv};
-use zkvm::{Anchor,Commitment, CommitmentWitness, Contract, Data, Predicate};
+use zkvm::{Anchor,Commitment, CommitmentWitness, Contract, Data, PortableItem, Value, Predicate};
 
 #[derive(Copy, Clone, Eq, Hash, Debug, PartialEq, Default)]
 pub struct ReceiverID([u8; 32]);
 
 /// State of the account
+#[derive(Clone, Debug)]
 pub struct Account {
     pub xpub: Xpub,
     pub sequence: u64,
 }
 
 /// Receiver describes the destination for the payment.
+#[derive(Clone, Debug)]
 pub struct Receiver {
     pub predicate: Predicate,
     pub qty: u64,
@@ -89,17 +92,73 @@ pub struct Receiver {
 }
 
 /// Contains the anchor for the contract that allows computing the ContractID.
+#[derive(Clone, Debug)]
 pub struct ContractInfo {
+    /// ID of the receiver to which this info applies
     pub receiver_id: ReceiverID,
+
+    /// Contract's anchor necessary to compute the contract ID
     pub anchor: Anchor,
 }
 
 impl Receiver {
 
+    /// Returns the unique identifier of the receiver.
     pub fn id(&self) -> ReceiverID {
-        unimplemented!()
+        let mut t = Transcript::new(b"ZkVM.accounts.receiver");
+        t.append_message(b"predicate", self.predicate.to_point().as_bytes());
+        t.append_u64(b"qty", self.qty);
+        t.append_message(b"flv", self.flv.as_bytes());
+        t.append_message(b"qty_blinding", self.qty_blinding.as_bytes());
+        t.append_message(b"flv_blinding", self.flv_blinding.as_bytes());
+        t.append_u64(b"expiration_ms", self.expiration_ms);
+        let mut receiver = ReceiverID([0u8;32]);
+        t.challenge_bytes(b"receiver_id", &mut receiver.0);
+        receiver
+    }
+
+    /// Constructs a value object from the qty, flavor and blinding factors.
+    pub fn value(&self) -> Value {
+        Value {
+            qty: Commitment::blinded_with_factor(self.qty, self.qty_blinding),
+            flv: Commitment::blinded_with_factor(self.flv, self.flv_blinding),
+        }
+    }
+
+    /// Creates a new contract for the given receiver and an anchor.
+    pub fn contract(&self, anchor: Anchor) -> Contract {
+        Contract::new(
+            self.predicate.clone(),
+            vec![PortableItem::Value(self.value())],
+            anchor
+        )
     }
 }
 
+impl Account {
 
+    /// Creates a new receiver and increments the sequence number
+    pub fn generate_receiver(&mut self, qty: u64, flv: Scalar, expiration_ms: u64) -> Receiver {
+        let key = self.xpub.derive_key(|t| t.append_u64(b"sequence", self.sequence) );
+        let (qty_blinding, flv_blinding) = self.make_blinding_factors();
+        let receiver = Receiver {
+            predicate: Predicate::Key(key),
+            qty,
+            flv,
+            qty_blinding,
+            flv_blinding,
+            expiration_ms
+        };
+        self.sequence += 1;
+        receiver
+    }
 
+    fn make_blinding_factors(&self) -> (Scalar, Scalar) {
+        let mut rng = Transcript::new(b"ZkVM.accounts.blinding")
+            .build_rng()
+            .rekey_with_witness_bytes(b"xpub", &self.xpub.to_bytes())
+            .finalize(&mut rand::thread_rng());
+        
+        (Scalar::random(&mut rng),Scalar::random(&mut rng))
+    }
+}
