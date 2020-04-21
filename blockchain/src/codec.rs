0a3594407864373aa354c5ddbadef589486a38a8
p0lunin <dmytro.polunin@gmail.com>
2020-04-21 14:38:58 +0300

    completed work on adding coding of blockchain::Message

--- blockchain/src/codec.rs
+++ blockchain/src/codec.rs
@@ -1,18 +1,18 @@
-use crate::{utreexo, Block, BlockHeader, BlockID, BlockTx, Message};
-use curve25519_dalek::ristretto::CompressedRistretto;
-use curve25519_dalek::scalar::Scalar;
+use crate::{Block, GetBlock, GetInventory, GetMempoolTxs, MempoolTxs, Message};
 use p2p::{
     reexport::{Buf, BufMut, Bytes, BytesMut},
     CustomMessage,
 };
-use std::convert::{Infallible, TryFrom, TryInto};
-use zkvm::bulletproofs::r1cs::{R1CSError, R1CSProof};
-use zkvm::{Hash, Signature, Tx, TxHeader};
+use std::convert::TryFrom;
 
 #[repr(u8)]
 enum MessageType {
     Block = 0,
-    F = 1,
+    GetBlock = 1,
+    Inventory = 2,
+    GetInventory = 3,
+    MempoolTxs = 4,
+    GetMempoolTxs = 5,
 }
 
 impl TryFrom<u8> for MessageType {
@@ -21,6 +21,11 @@ impl TryFrom<u8> for MessageType {
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0 => Ok(MessageType::Block),
+            1 => Ok(MessageType::GetBlock),
+            2 => Ok(MessageType::Inventory),
+            3 => Ok(MessageType::GetInventory),
+            4 => Ok(MessageType::MempoolTxs),
+            5 => Ok(MessageType::GetMempoolTxs),
             _ => Err(format!("unknown message type: {}", value)),
         }
     }
@@ -37,16 +42,44 @@ impl CustomMessage for Message {
         let message_type = MessageType::try_from(message_type_byte)?;
         match message_type {
             MessageType::Block => {
-                let header = decode_block_header(src)?;
-                let signature = decode_signature(src)?;
-                let txs = decode_txs(src)?;
+                let header = block_header::decode(src)?;
+                let signature = signature::decode(src)?;
+                let txs = txs::decode(src)?;
                 Ok(Message::Block(Block {
                     header,
                     signature,
                     txs,
                 }))
             }
-            _ => unimplemented!(),
+            MessageType::GetBlock => {
+                let height = get_u64_le(src, "height of block")?;
+                Ok(Message::GetBlock(GetBlock { height }))
+            }
+            MessageType::Inventory => {
+                let inventory = inventory::decode(src)?;
+                Ok(Message::Inventory(inventory))
+            }
+            MessageType::GetInventory => {
+                let version = get_u64_le(src, "inventory version")?;
+                let shortid_nonce = get_u64_le(src, "shortid_nonce")?;
+                Ok(Message::GetInventory(GetInventory {
+                    version,
+                    shortid_nonce,
+                }))
+            }
+            MessageType::MempoolTxs => {
+                let tip = block_id::decode(src)?;
+                let txs = txs::decode(src)?;
+                Ok(Message::MempoolTxs(MempoolTxs { tip, txs }))
+            }
+            MessageType::GetMempoolTxs => {
+                let shortid_nonce = get_u64_le(src, "shortid_nonce")?;
+                let shortid_list = shortid_list::decode(src)?;
+                Ok(Message::GetMempoolTxs(GetMempoolTxs {
+                    shortid_nonce,
+                    shortid_list,
+                }))
+            }
         }
     }
 
@@ -54,55 +87,242 @@ impl CustomMessage for Message {
         match self {
             Message::Block(b) => {
                 dst.put_u8(MessageType::Block as u8); // Message type
-                encode_block_header(&b.header, dst);
-                encode_signature(&b.signature, dst);
-                encode_txs(b.txs, dst);
+                block_header::encode(&b.header, dst);
+                signature::encode(&b.signature, dst);
+                txs::encode(b.txs, dst);
             }
-            _ => unimplemented!(),
+            Message::GetBlock(g) => {
+                dst.put_u8(MessageType::GetBlock as u8);
+                dst.put_u64_le(g.height);
+            }
+            Message::Inventory(inv) => {
+                dst.put_u8(MessageType::Inventory as u8);
+                inventory::encode(inv, dst);
+            }
+            Message::GetInventory(g) => {
+                dst.put_u8(MessageType::GetInventory as u8);
+                dst.put_u64_le(g.version);
+                dst.put_u64_le(g.shortid_nonce);
+            }
+            Message::MempoolTxs(mempool) => {
+                block_id::encode(&mempool.tip, dst);
+                txs::encode(mempool.txs, dst);
+            }
+            Message::GetMempoolTxs(g) => {
+                dst.put_u64_le(g.shortid_nonce);
+                shortid_list::encode(&g.shortid_list, dst);
+            }
         }
         Ok(())
     }
 }
 
-fn encode_block_header(header: &BlockHeader, dst: &mut BytesMut) {
-    dst.put_u64_le(header.version);
-    dst.put_u64_le(header.height);
-    dst.put(&header.prev.0[..]);
-    dst.put_u64_le(header.timestamp_ms);
-    dst.put(header.txroot.0.as_ref());
-    dst.put(header.utxoroot.0.as_ref());
-    encode_u8_slice(header.ext.as_slice(), dst);
+mod block_header {
+    use crate::codec::{block_id, check_length, decode_u8_vec, encode_u8_slice};
+    use crate::BlockHeader;
+    use p2p::reexport::{Buf, BufMut, Bytes, BytesMut};
+    use std::convert::TryInto;
+    use zkvm::Hash;
+
+    pub fn encode(header: &BlockHeader, dst: &mut BytesMut) {
+        dst.put_u64_le(header.version);
+        dst.put_u64_le(header.height);
+        dst.put(&header.prev.0[..]);
+        dst.put_u64_le(header.timestamp_ms);
+        dst.put(header.txroot.0.as_ref());
+        dst.put(header.utxoroot.0.as_ref());
+        encode_u8_slice(header.ext.as_slice(), dst);
+    }
+
+    const BLOCK_HEADER_SIZE: usize = 8 + 8 + 32 + 8 + 32 + 32;
+    pub fn decode(src: &mut Bytes) -> Result<BlockHeader, String> {
+        check_length(src, BLOCK_HEADER_SIZE, "block header")?;
+        let version = src.get_u64_le();
+        let height = src.get_u64_le();
+        let prev = block_id::decode(src)?;
+        let timestamp_ms = src.get_u64_le();
+        let txroot = src.split_to(32);
+        let utxoroot = src.split_to(32);
+        let ext = decode_u8_vec(src, "ext")?;
+        Ok(BlockHeader {
+            version,
+            height,
+            prev,
+            timestamp_ms,
+            txroot: Hash(txroot.as_ref().try_into().unwrap()),
+            utxoroot: Hash(utxoroot.as_ref().try_into().unwrap()),
+            ext,
+        })
+    }
 }
 
-fn encode_signature(signature: &Signature, dst: &mut BytesMut) {
-    dst.put(signature.s.as_bytes().as_ref());
-    dst.put(signature.R.as_bytes().as_ref());
+mod signature {
+    use crate::codec::check_length;
+    use curve25519_dalek::ristretto::CompressedRistretto;
+    use curve25519_dalek::scalar::Scalar;
+    use p2p::reexport::{BufMut, Bytes, BytesMut};
+    use std::convert::TryInto;
+    use zkvm::Signature;
+
+    pub fn encode(signature: &Signature, dst: &mut BytesMut) {
+        dst.put(signature.s.as_bytes().as_ref());
+        dst.put(signature.R.as_bytes().as_ref());
+    }
+
+    const SIGNATURE_SIZE: usize = 32 + 32;
+    pub fn decode(src: &mut Bytes) -> Result<Signature, String> {
+        check_length(src, SIGNATURE_SIZE, "signature")?;
+        let s = src.split_to(32).as_ref().try_into().unwrap();
+        let r = src.split_to(32).as_ref().try_into().unwrap();
+        Ok(Signature {
+            s: Scalar::from_bits(s),
+            R: CompressedRistretto(r),
+        })
+    }
 }
 
-fn encode_txs(txs: Vec<BlockTx>, dst: &mut BytesMut) {
-    fn encode_block_tx(block_tx: BlockTx, dst: &mut BytesMut) {
-        dst.put_u64_le(block_tx.tx.header.version);
-        dst.put_u64_le(block_tx.tx.header.mintime_ms);
-        dst.put_u64_le(block_tx.tx.header.maxtime_ms);
-        encode_u8_slice(block_tx.tx.program.as_slice(), dst);
-        encode_signature(&block_tx.tx.signature, dst);
-        encode_u8_slice(block_tx.tx.proof.to_bytes().as_slice(), dst);
-        fn encode_proof(proof: utreexo::Proof, dst: &mut BytesMut) {
-            match proof {
-                utreexo::Proof::Transient => dst.put_u8(0),
-                utreexo::Proof::Committed(path) => {
-                    dst.put_u8(1);
-                    dst.put_u64_le(path.position);
-                    fn encode_hash(hash: Hash, dst: &mut BytesMut) {
-                        dst.put(hash.0.as_ref());
+mod shortid_list {
+    use super::{decode_u8_vec, encode_u8_slice, Bytes, BytesMut};
+    use crate::shortid::ShortIDVec;
+
+    pub fn decode(src: &mut Bytes) -> Result<ShortIDVec, String> {
+        let shortid_list = decode_u8_vec(src, "shortid_list")?;
+        Ok(ShortIDVec(shortid_list))
+    }
+
+    pub fn encode(vec: &ShortIDVec, dst: &mut BytesMut) {
+        encode_u8_slice(vec.0.as_slice(), dst);
+    }
+}
+
+mod block_id {
+    use super::{check_length, encode_u8_slice, Bytes, BytesMut};
+    use crate::BlockID;
+    use std::convert::TryInto;
+
+    pub fn decode(src: &mut Bytes) -> Result<BlockID, String> {
+        check_length(src, 32, "block id")?;
+        let bytes = src.split_to(32);
+        Ok(BlockID(bytes.as_ref().try_into().unwrap()))
+    }
+
+    pub fn encode(block_id: &BlockID, dst: &mut BytesMut) {
+        encode_u8_slice(block_id.0.as_ref(), dst);
+    }
+}
+
+mod txs {
+    use crate::codec::{
+        check_length, decode_u8_vec, decode_vector, encode_u8_slice, encode_vector, get_array,
+        get_u64_le, get_u8, signature,
+    };
+    use crate::{utreexo, BlockTx};
+    use p2p::reexport::{Buf, BufMut, Bytes, BytesMut};
+    use std::convert::TryInto;
+    use zkvm::bulletproofs::r1cs::R1CSProof;
+    use zkvm::{Hash, Tx, TxHeader};
+
+    pub fn encode(txs: Vec<BlockTx>, dst: &mut BytesMut) {
+        fn encode_block_tx(block_tx: BlockTx, dst: &mut BytesMut) {
+            dst.put_u64_le(block_tx.tx.header.version);
+            dst.put_u64_le(block_tx.tx.header.mintime_ms);
+            dst.put_u64_le(block_tx.tx.header.maxtime_ms);
+            encode_u8_slice(block_tx.tx.program.as_slice(), dst);
+            signature::encode(&block_tx.tx.signature, dst);
+            encode_u8_slice(block_tx.tx.proof.to_bytes().as_slice(), dst);
+            fn encode_proof(proof: utreexo::Proof, dst: &mut BytesMut) {
+                match proof {
+                    utreexo::Proof::Transient => dst.put_u8(0),
+                    utreexo::Proof::Committed(path) => {
+                        dst.put_u8(1);
+                        dst.put_u64_le(path.position);
+                        fn encode_hash(hash: Hash, dst: &mut BytesMut) {
+                            dst.put(hash.0.as_ref());
+                        }
+                        encode_vector(path.neighbors, dst, encode_hash);
                     }
-                    encode_vector(path.neighbors, dst, encode_hash);
                 }
             }
+            encode_vector(block_tx.proofs, dst, encode_proof);
         }
-        encode_vector(block_tx.proofs, dst, encode_proof);
+        encode_vector(txs, dst, encode_block_tx)
     }
-    encode_vector(txs, dst, encode_block_tx)
+
+    pub fn decode(src: &mut Bytes) -> Result<Vec<BlockTx>, String> {
+        fn decode_block_tx(src: &mut Bytes) -> Result<BlockTx, String> {
+            const TX_HEADER_LENGTH: usize = 8 + 8 + 8;
+            check_length(src, TX_HEADER_LENGTH, "tx header")?;
+            let header = TxHeader {
+                version: src.get_u64_le(),
+                mintime_ms: src.get_u64_le(),
+                maxtime_ms: src.get_u64_le(),
+            };
+            let program = decode_u8_vec(src, "program")?;
+            let signature = signature::decode(src)?;
+            let proof_bytes = get_array(src, "r1cs proof")?;
+            let proof = R1CSProof::from_bytes(proof_bytes.as_ref())
+                .map_err(|_| "an error was occur when parse r1cs proof")?;
+
+            let tx = Tx {
+                header,
+                program,
+                signature,
+                proof,
+            };
+
+            fn decode_utreexo_proof(src: &mut Bytes) -> Result<utreexo::Proof, String> {
+                let proof_type = get_u8(src, "proof")?;
+                match proof_type {
+                    0 => Ok(utreexo::Proof::Transient),
+                    1 => {
+                        let position = get_u64_le(src, "path position")?;
+                        fn decode_hash(src: &mut Bytes) -> Result<Hash, String> {
+                            check_length(src, 32, "utreexo proof hash")?;
+                            Ok(Hash(src.split_to(32).as_ref().try_into().unwrap()))
+                        }
+                        let neighbors = decode_vector(src, "utreexo proof", decode_hash)?;
+                        Ok(utreexo::Proof::Committed(zkvm::merkle::Path {
+                            position,
+                            neighbors,
+                        }))
+                    }
+                    _ => Err(format!("unknown proof type: {}", proof_type)),
+                }
+            }
+            let proofs = decode_vector(src, "utreexo proofs", decode_utreexo_proof)?;
+            Ok(BlockTx { tx, proofs })
+        }
+        decode_vector(src, "block txs", decode_block_tx)
+    }
+}
+
+mod inventory {
+    use crate::codec::{block_header, get_u64_le, shortid_list, signature};
+    use crate::Inventory;
+    use p2p::reexport::{BufMut, Bytes, BytesMut};
+
+    pub fn encode(inv: Inventory, dst: &mut BytesMut) {
+        dst.put_u64_le(inv.version);
+        block_header::encode(&inv.tip, dst);
+        signature::encode(&inv.tip_signature, dst);
+        dst.put_u64_le(inv.shortid_nonce);
+        shortid_list::encode(&inv.shortid_list, dst);
+    }
+
+    pub fn decode(src: &mut Bytes) -> Result<Inventory, String> {
+        let version = get_u64_le(src, "version")?;
+        let tip = block_header::decode(src)?;
+        let tip_signature = signature::decode(src)?;
+        let shortid_nonce = get_u64_le(src, "shortid_nonce")?;
+        let shortid_list = shortid_list::decode(src)?;
+        Ok(Inventory {
+            version,
+            tip,
+            tip_signature,
+            shortid_nonce,
+            shortid_list,
+        })
+    }
 }
 
 fn encode_vector<T, F: Fn(T, &mut BytesMut)>(vec: Vec<T>, dst: &mut BytesMut, f: F) {
@@ -118,90 +338,10 @@ fn encode_u8_slice(vec: &[u8], dst: &mut BytesMut) {
     dst.put(vec);
 }
 
-const BLOCK_HEADER_SIZE: usize = 8 + 8 + 32 + 8 + 32 + 32;
-fn decode_block_header(src: &mut Bytes) -> Result<BlockHeader, String> {
-    check_length(src, BLOCK_HEADER_SIZE, "block header")?;
-    let version = src.get_u64_le();
-    let height = src.get_u64_le();
-    let prev = src.split_to(32);
-    let timestamp_ms = src.get_u64_le();
-    let txroot = src.split_to(32);
-    let utxoroot = src.split_to(32);
-    let ext = decode_u8_vec(src, "ext")?;
-    Ok(BlockHeader {
-        version,
-        height,
-        prev: BlockID(prev.as_ref().try_into().unwrap()),
-        timestamp_ms,
-        txroot: Hash(txroot.as_ref().try_into().unwrap()),
-        utxoroot: Hash(utxoroot.as_ref().try_into().unwrap()),
-        ext,
-    })
-}
-
-const SIGNATURE_SIZE: usize = 32 + 32;
-fn decode_signature(src: &mut Bytes) -> Result<Signature, String> {
-    check_length(src, SIGNATURE_SIZE, "signature")?;
-    let s = src.split_to(32).as_ref().try_into().unwrap();
-    let r = src.split_to(32).as_ref().try_into().unwrap();
-    Ok(Signature {
-        s: Scalar::from_bits(s),
-        R: CompressedRistretto(r),
-    })
-}
-
-fn decode_txs(src: &mut Bytes) -> Result<Vec<BlockTx>, String> {
-    fn decode_block_tx(src: &mut Bytes) -> Result<BlockTx, String> {
-        const TX_HEADER_LENGTH: usize = 8 + 8 + 8;
-        check_length(src, TX_HEADER_LENGTH, "tx header")?;
-        let header = TxHeader {
-            version: src.get_u64_le(),
-            mintime_ms: src.get_u64_le(),
-            maxtime_ms: src.get_u64_le(),
-        };
-        let program = decode_u8_vec(src, "program")?;
-        let signature = decode_signature(src)?;
-        let proof_bytes = get_array(src, "r1cs proof")?;
-        let proof = R1CSProof::from_bytes(proof_bytes.as_ref())
-            .map_err(|_| "an error was occur when parse r1cs proof")?;
-
-        let tx = Tx {
-            header,
-            program,
-            signature,
-            proof,
-        };
-
-        fn decode_utreexo_proof(src: &mut Bytes) -> Result<utreexo::Proof, String> {
-            let proof_type = get_u8(src, "proof")?;
-            match proof_type {
-                0 => Ok(utreexo::Proof::Transient),
-                1 => {
-                    let position = get_u64_le(src, "path position")?;
-                    fn decode_hash(src: &mut Bytes) -> Result<Hash, String> {
-                        check_length(src, 32, "utreexo proof hash")?;
-                        Ok(Hash(src.split_to(32).as_ref().try_into().unwrap()))
-                    }
-                    let neighbors = decode_vector(src, "utreexo proof", decode_hash)?;
-                    Ok(utreexo::Proof::Committed(zkvm::merkle::Path {
-                        position,
-                        neighbors,
-                    }))
-                }
-                _ => Err(format!("unknown proof type: {}", proof_type)),
-            }
-        }
-        let proofs = decode_vector(src, "utreexo proofs", decode_utreexo_proof)?;
-        Ok(BlockTx { tx, proofs })
-    }
-    decode_vector(src, "block txs", decode_block_tx)
-}
-
-fn decode_vector<T, F: Fn(&mut Bytes) -> Result<T, String>>(
-    src: &mut Bytes,
-    label: &str,
-    parse: F,
-) -> Result<Vec<T>, String> {
+fn decode_vector<T, F>(src: &mut Bytes, label: &str, parse: F) -> Result<Vec<T>, String>
+where
+    F: Fn(&mut Bytes) -> Result<T, String>,
+{
     let mut vec_bytes = get_array(src, label)?;
     let mut vec = vec![];
     while !vec_bytes.is_empty() {
@@ -247,7 +387,11 @@ fn check_length<'a, T: Into<&'a str>>(buf: &Bytes, len: usize, label: T) -> Resu
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fmt::Debug;
+    use crate::{utreexo, BlockHeader, BlockID, BlockTx};
+    use curve25519_dalek::ristretto::CompressedRistretto;
+    use curve25519_dalek::scalar::Scalar;
+    use zkvm::bulletproofs::r1cs::R1CSProof;
+    use zkvm::{Hash, Signature, Tx, TxHeader};
 
     #[test]
     fn message_block() {
@@ -288,6 +432,20 @@ mod tests {
                 ],
             }],
         });
+        let mut bytes = BytesMut::new();
+        message.clone().encode(&mut bytes).unwrap();
+        let mut bytes = bytes.freeze();
+        let res = Message::decode(&mut bytes).unwrap();
+        assert!(bytes.is_empty());
+
+        let left = format!("{:?}", message);
+        let right = format!("{:?}", res);
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn message_get_block() {
+        let message = Message::GetBlock(GetBlock { height: 30 });
         let mut bytes = BytesMut::new();
         message.clone().encode(&mut bytes).unwrap();
         let mut bytes = bytes.freeze();
