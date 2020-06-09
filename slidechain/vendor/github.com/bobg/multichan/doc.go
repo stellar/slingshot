002e65b812e852074bc83c88daad09ae5d1a1d21
Oleg Andreev <oleganza@gmail.com>
2020-06-09 15:19:36 +0200

    slidechain: remove outdated code (#466)

--- slidechain/vendor/github.com/bobg/multichan/doc.go
+++
@@ -1,9 +0,0 @@
-// Package multichan provides a one-to-many data channel.
-//
-// The source of some data creates a writer (type multichan.W)
-// and supplies items to it one at a time with W.Write.
-//
-// Consumers of those items create readers with W.Reader
-// (producing a multichan.R)
-// and read items with R.Read and R.NBRead.
-package multichan
--- slidechain/vendor/github.com/chain/txvm/protocol/txvm/time.go
+++
@@ -1,7 +0,0 @@
-package txvm
-
-func opTimeRange(vm *VM) {
-	max := vm.popInt()
-	min := vm.popInt()
-	vm.logTimeRange(min, max)
-}
